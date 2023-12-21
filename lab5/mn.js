d450c8a1c48864a88346fb7efeb5ab4f1f2bad1a
masha kulakova <mashakulakova04@inbox.ru>
2023-12-21 19:19:16 +0300

    abv

--- lab5/mn.js
+++ lab5/mn.js
@@ -1,85 +1,208 @@
-'use strict';
-
-const BASE_URL = new URL("http://cat-facts-api.std-900.ist.mospolytech.ru/");
-
-const state = {
-    page: 1,
-    perPage: 10,
-    pageCount: 0
+function createAuthorElement(record) {
+    let user = record.user || { 'name': { 'first': '', 'last': '' } };
+    let authorElement = document.createElement('div');
+    authorElement.classList.add('author-name');
+    authorElement.innerHTML = user.name.first + ' ' + user.name.last;
+    return authorElement;
 }
 
+function createUpvotesElement(record) {
+    let upvotesElement = document.createElement('div');
+    upvotesElement.classList.add('upvotes');
+    upvotesElement.innerHTML = record.upvotes;
+    return upvotesElement;
+}
 
-const fillPageByCollection = (collection) => {
-    let template = document.getElementById("post_t");
-    const main = document.querySelector(".posts");
-    for (let record of collection) {
-        let post = template.content.cloneNode(true);
-        post.querySelector("p").textContent = record.text;
-        post.querySelector(".userCard").textContent = 
-            record?.user?.name?.first +
-            " " +
-            record?.user?.name?.last;
-        main.append(post);
+function createFooterElement(record) {
+    let footerElement = document.createElement('div');
+    footerElement.classList.add('item-footer');
+    footerElement.append(createAuthorElement(record));
+    footerElement.append(createUpvotesElement(record));
+    return footerElement;
+}
+
+function createContentElement(record) {
+    let contentElement = document.createElement('div');
+    contentElement.classList.add('item-content');
+    contentElement.innerHTML = record.text;
+    return contentElement;
+}
+
+function createListItemElement(record) {
+    let itemElement = document.createElement('div');
+    itemElement.classList.add('facts-list-item');
+    itemElement.append(createContentElement(record));
+    itemElement.append(createFooterElement(record));
+    return itemElement;
+}
+
+function renderRecords(records) {
+    let factsList = document.querySelector('.facts-list');
+    factsList.innerHTML = '';
+    for (let i = 0; i < records.length; i++) {
+        factsList.append(createListItemElement(records[i]));
     }
-};
-
-function pageCounter(objectPagination) {
-    state.pageCount = objectPagination.total_pages;
 }
 
-function numeration() {
-    let first = Math.max(1, state.page - 2);
-    let btnp = document.getElementById('btn-p');
-    let last = Math.min(state.page + 2, state.pageCount);
-    btnp.innerHTML = "";
-    for (let i = first; i <= last; i++) {
-        let btn = document.createElement("button");
-        btn.innerHTML = i;
-        btn.onclick = () => goToPage(i);
-        btnp.append(btn);
+function setPaginationInfo(info) {
+    document.querySelector('.total-count').innerHTML = info.total_count;
+    let start = info.total_count && (info.current_page - 1) * info.per_page + 1;
+    document.querySelector('.current-interval-start').innerHTML = start;
+    let end = Math.min(info.total_count, start + info.per_page - 1);
+    document.querySelector('.current-interval-end').innerHTML = end;
+}
+
+function createPageBtn(page, classes = []) {
+    let btn = document.createElement('button');
+    classes.push('btn');
+    for (cls of classes) {
+        btn.classList.add(cls);
     }
+    btn.dataset.page = page;
+    btn.innerHTML = page;
+    return btn;
 }
 
-function getRequest(page = 1, perPage = 10) {
-    let url = new URL("facts", BASE_URL);
-    url.searchParams.set("page", page);
-    url.searchParams.set("per-page", perPage);
+function renderPaginationElement(info) {
+    let btn;
+    let paginationContainer = document.querySelector('.pagination');
+    paginationContainer.innerHTML = '';
 
+    btn = createPageBtn(1, ['first-page-btn']);
+    btn.innerHTML = 'Первая страница';
+    if (info.current_page == 1) {
+        btn.style.visibility = 'hidden';
+    }
+    paginationContainer.append(btn);
+
+    let buttonsContainer = document.createElement('div');
+    buttonsContainer.classList.add('pages-btns');
+    paginationContainer.append(buttonsContainer);
+
+    let start = Math.max(info.current_page - 2, 1);
+    let end = Math.min(info.current_page + 2, info.total_pages);
+    for (let i = start; i <= end; i++) {
+        btn = createPageBtn(i, i == info.current_page ? ['active'] : []);
+        buttonsContainer.append(btn);
+    }
+
+    btn = createPageBtn(info.total_pages, ['last-page-btn']);
+    btn.innerHTML = 'Последняя страница';
+    if (info.current_page == info.total_pages) {
+        btn.style.visibility = 'hidden';
+    }
+    paginationContainer.append(btn);
+}
+
+function downloadData(page = 1) {
+    let factsList = document.querySelector('.facts-list');
+    let url = new URL(factsList.dataset.url);
+    let perPage = document.querySelector('.per-page-btn').value;
+    url.searchParams.append('page', page);
+    url.searchParams.append('per-page', perPage);
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.send();
-
-    xhr.onload = function() {
-        //alert(`Есть: ${xhr.status}, ${xhr.response}`);
-        let json = JSON.parse(xhr.response);
-        cleanPosts();
-        fillPageByCollection(json.records);
-        pageCounter(json._pagination);
-        numeration();
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        renderRecords(this.response.records);
+        setPaginationInfo(this.response['_pagination']);
+        renderPaginationElement(this.response['_pagination']);
     };
+    xhr.send();
 }
 
-function cleanPosts() {
-    let main = document.querySelector(".posts");
-    main.innerHTML = "";
+function perPageBtnHandler(event) {
+    downloadData(1);
 }
 
-window.onload = () => {
-    getRequest(state.page, state.perPage);
+function pageBtnHandler(event) {
+    if (event.target.dataset.page) {
+        downloadData(event.target.dataset.page);
+        window.scrollTo(0, 0);
+    }
+}
+
+window.onload = function () {
+    downloadData();
+    document.querySelector('.pagination').onclick = pageBtnHandler;
+    document.querySelector('.per-page-btn').onchange = perPageBtnHandler;
 };
 
-function goToPage(pageNum) {
-    state.page = pageNum;
-    getRequest(state.page, state.perPage)
+document.querySelector('.pagination').onclick = pageBtnHandler;
+document.querySelector('.per-page-btn').onchange = perPageBtnHandler;
 
+document.querySelector('.search-btn').onclick = searchBtnHandler;
+
+// Add event listener for the search field to enable autocomplete
+document.querySelector('.search-field').addEventListener('input', autocompleteHandler);
+
+// ... (previous code)
+
+function searchBtnHandler() {
+const searchField = document.getElementById('searchField');
+const searchQuery = searchField.value.trim();
+if (searchQuery !== "") {
+    downloadData(1, searchQuery);
+    document.getElementById('autocompleteDropdown').innerHTML = ""; // Clear autocomplete dropdown
+}
 }
 
-function getNextPage() {
-    state.page++;
-    getRequest(state.page, state.perPage);
+function autocompleteHandler(event) {
+const searchField = document.getElementById('searchField');
+const autocompleteDropdown = document.getElementById('autocompleteDropdown');
+const searchQuery = searchField.value.trim();
+
+if (searchQuery !== "") {
+    const rect = searchField.getBoundingClientRect();
+    autocompleteDropdown.style.width = rect.width + 'px';
+    autocompleteDropdown.style.top = rect.bottom + 'px';
+    autocompleteDropdown.style.left = rect.left + 'px';
+
+    fetch(`http://cat-facts-api.std-900.ist.mospolytech.ru/autocomplete?q=${searchQuery}`)
+        .then(response => response.json())
+        .then(data => {
+            autocompleteDropdown.innerHTML = "";
+            data.forEach(suggestion => {
+                const suggestionItem = document.createElement('div');
+                suggestionItem.classList.add('autocomplete-item');
+                suggestionItem.innerText = suggestion;
+                suggestionItem.addEventListener('click', () => {
+                    searchField.value = suggestion;
+                    autocompleteDropdown.innerHTML = "";
+                    searchBtnHandler();
+                });
+                autocompleteDropdown.appendChild(suggestionItem);
+            });
+        })
+        .catch(error => console.error('Autocomplete error:', error));
+} else {
+    autocompleteDropdown.innerHTML = "";
+}
 }
 
-function getBackPage() {
-    state.page--;
-    getRequest(state.page, state.perPage);   
+function downloadData(page = 1, searchQuery = "") {
+let factsList = document.querySelector('.facts-list');
+let url = new URL(factsList.dataset.url);
+let perPage = document.querySelector('.per-page-btn').value;
+url.searchParams.append('page', page);
+url.searchParams.append('per-page', perPage);
+url.searchParams.append('q', searchQuery); // Add the 'q' parameter for search
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', url);
+xhr.responseType = 'json';
+xhr.onload = function () {
+    renderRecords(this.response.records);
+    setPaginationInfo(this.response['_pagination']);
+    renderPaginationElement(this.response['_pagination']);
+};
+xhr.send();
 }
+
+window.onload = function () {
+downloadData();
+document.querySelector('.pagination').onclick = pageBtnHandler;
+document.querySelector('.per-page-btn').onchange = perPageBtnHandler;
+document.querySelector('.search-btn').onclick = searchBtnHandler;
+document.getElementById('searchField').addEventListener('input', autocompleteHandler);
+};
